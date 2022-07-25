0ae32368c5aa5b13823dca2d95248d0ead02a30b
Masaki Kawabata Neto <kawabata@usp.br>
2022-07-25 16:01:12 -0300

    consulta status

--- testes/consultaStatus.php
+++ testes/consultaStatus.php
@@ -10,47 +10,43 @@ use NFePHP\Common\Certificate;
 use NFePHP\NFe\Common\Standardize;
 use NFePHP\NFe\Common\Complements;
 
+
+echo "OS: " . PHP_OS . "\n";
+echo "uname: " . php_uname() . "\n";
+echo "PHP version: " . phpversion() . "\n";
+$curl_version = curl_version();
+echo "curl version: " . $curl_version["version"] . "\n";
+echo "SSL version: " . $curl_version["ssl_version"] . "\n";
+echo "SSL version number: " . $curl_version["ssl_version_number"] . "\n";
+echo "OPENSSL_VERSION_NUMBER: " . dechex(OPENSSL_VERSION_NUMBER) . "\n";
+
 $arr = [
     "atualizacao" => "2022-07-25 10:26:00",
     "tpAmb" => 1, // producao
     "razaosocial" => "Escola de Engenharia de São Carlos",
     "cnpj" => "63025530002824",
     "siglaUF" => "SP",
-    //"schemes" => "PL008i2",
     "schemes" => "PL009_V4",
     "versao" => '4.00'
 ];
-//monta o config.json
 $configJson = json_encode($arr);
-//carrega o conteudo do certificado.
-$content = file_get_contents($cfg['cert_file']);
 
 try {
 
-    $certificate = Certificate::readPfx($content,  $cfg['cert_pwd']);
+    $certificate = Certificate::readPfx(file_get_contents($cfg['cert_file']),  $cfg['cert_pwd']);
     $tools = new Tools($configJson, $certificate);
-    $tools->model('55');
-    $uf = 'SP';
-    $tpAmb = 2;
-    $response = $tools->sefazStatus($uf, $tpAmb);
-    //este método não requer parametros, são opcionais, se nenhum parametro for 
-    //passado serão usados os contidos no $configJson
-    //$response = $tools->sefazStatus();
 
-    //você pode padronizar os dados de retorno atraves da classe abaixo
-    //de forma a facilitar a extração dos dados do XML
-    //NOTA: mas lembre-se que esse XML muitas vezes será necessário, 
-    //      quando houver a necessidade de protocolos
+    $soap = new NFePHP\Common\Soap\SoapCurl($certificate);
+    $soap->protocol($soap::SSL_TLSV1_2);
+    $tools->loadSoapClass($soap);
+
+    $tools->model('55');
+    $response = $tools->sefazStatus();
+
     $stdCl = new Standardize($response);
-    //nesse caso $std irá conter uma representação em stdClass do XML
-    $std = $stdCl->toStd();
-    //nesse caso o $arr irá conter uma representação em array do XML
-    $arr = $stdCl->toArray();
-    //nesse caso o $json irá conter uma representação em JSON do XML
     $json = $stdCl->toJson();
-    
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
 
-echo $json;
+echo 'json: ', $json, "\n";
